336ea92f2c5be3930d12f89d047bc3e031c32fb4
Debopriyo Ghosh <debopriyodjghosh@gmail.com>
2021-02-17 13:27:45 +0530

    authentication removed

--- admin/lib/config.php
+++ admin/lib/config.php
@@ -4,10 +4,10 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 // DATABASE SETTINGS
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'shibpmpw_web');
+define('DB_NAME', 'database');
 define('DB_CHARSET', 'utf8');
-define('DB_USER', 'shibpmpw_develop');
-define('DB_PASSWORD', 'new2020');
+define('DB_USER', 'user');
+define('DB_PASSWORD', 'password');
 
 // FILE PATHS
 define("PATH_LIB", __DIR__ . DIRECTORY_SEPARATOR);
